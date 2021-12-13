1dae27d28da24661971c5b2e91d2bbaef3d152e8
unknown <rutujakhaire835@gmail.com>
2021-12-13 17:51:21 +0530

    The Messenger App

---
+++ lib/views/signin.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:matcher/matcher.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({ Key? key }) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Messenger Clone"),
+        ),
+      
+    );
+  }
+}
